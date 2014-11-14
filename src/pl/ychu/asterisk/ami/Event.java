9ba37bf2f25aea61f0c252330e6f1f71f22f5f8b
krzysztof <krz@ychu.pl>
2014-11-14 20:12:20 +0100

    Implementacja klas Event i Response

--- src/pl/ychu/asterisk/ami/Event.java
+++ src/pl/ychu/asterisk/ami/Event.java
@@ -1,23 +1,39 @@
 package pl.ychu.asterisk.ami;
 
+import java.util.HashMap;
+
 /**
  * Created by Krzysztof on 2014-11-10.
  */
 public class Event {
 
-    private String eventMsg;
+    private final String eventMsg;
+    private final HashMap<String, String> map;
 
     protected Event(String event) {
         this.eventMsg = event;
-        //TODO
+        this.map = new HashMap<String, String>();
+        for (String line : event.split("\n")) {
+            String[] lineS = line.split(":", 2);
+            if (line.equals("") || lineS.length == 1) {
+                continue;
+            }
+            map.put(lineS[0].trim(), lineS[1].trim());
+        }
     }
 
     public String getMessage() {
         return eventMsg;
     }
 
-    //TODO
-    @Deprecated
+    public String getEventName() {
+        return this.getVariable("Event");
+    }
+
+    public String getVariable(String variableName) {
+        return map.get(variableName);
+    }
+
     public static Event parseEvent(String message) {
         Event e = new Event(message);
         return e;
--- src/pl/ychu/asterisk/ami/Response.java
+++ src/pl/ychu/asterisk/ami/Response.java
@@ -10,17 +10,16 @@ public class Response {
     private HashMap<String, String> vars;
     private String addrInfo;
 
-    //TODO
+
     public Response(String message) {
         this.message = message;
-        String[] msgSplited = message.split("\n");
         StringBuilder sb = new StringBuilder(1000);
         this.vars = new HashMap<String, String>();
-        for (String line : msgSplited) {
+        for (String line : message.split("\n")) {
             if (line.endsWith("\r") && line.length() != 0) {
-                String[] lineSplited = line.split(":", 2);
-                if (lineSplited.length == 2) {
-                    vars.put(lineSplited[0].trim(), lineSplited[1].trim());
+                String[] lineS = line.split(":", 2);
+                if (lineS.length == 2) {
+                    vars.put(lineS[0].trim(), lineS[1].trim());
                 }
             } else {
                 sb.append(line);
@@ -36,15 +35,19 @@ public class Response {
         return message;
     }
 
-    public HashMap<String, String> getVars() {
-        return vars;
-    }
-
     public String getAdditionalData() {
         return addrInfo;
     }
 
+    public String getVariable(String variableName) {
+        return vars.get(variableName);
+    }
+
+    public String getResponseStatus() {
+        return this.getVariable("Response");
+    }
+
     public String getActionId() {
-        return vars.get("ActionID");
+        return this.getVariable("ActionID");
     }
 }
