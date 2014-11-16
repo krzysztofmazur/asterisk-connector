5dd24d72e6fde38a5b7e24ec3c1b43df0e700f88
krzysztof <krz@ychu.pl>
2014-11-16 09:02:58 +0100

    JavaDoc dla Configuration

--- src/pl/ychu/asterisk/ami/Configuration.java
+++ src/pl/ychu/asterisk/ami/Configuration.java
@@ -13,6 +13,14 @@ public class Configuration {
     protected Configuration() {
     }
 
+    /**
+     * Konstruktor obiektu służącego jako konfiguracja dla połączeń z asteriskiem.
+     *
+     * @param hostName
+     * @param hostPort
+     * @param userName
+     * @param userPassword
+     */
     public Configuration(String hostName, int hostPort, String userName, String userPassword) {
         this.hostName = hostName;
         this.userName = userName;
@@ -20,42 +28,84 @@ public class Configuration {
         this.hostPort = hostPort;
     }
 
+    /**
+     * Metoda zwraca informację czy jest w konfiguracji włączony wątek służący do podtrzymywania połączenia.
+     *
+     * @return true jeśli wątek jest włączony, false jeśli nie jest
+     */
     public boolean isEnabledMaintainingThread() {
         return runMaintainingThread;
     }
 
+    /**
+     * Metoda pozwala na ustawienie parametru decydującego za włączenie wątka do podrzymywania połączenia.
+     *
+     * @param runMaintainingThread True jeśli wątek ma być włączony, false jeśli nie.
+     */
     public void enableRunMaintainingThread(boolean runMaintainingThread) {
         this.runMaintainingThread = runMaintainingThread;
     }
 
+    /**
+     * @return Nazwa hosta.
+     */
     public String getHostName() {
         return hostName;
     }
 
+    /**
+     * Metoda pozwalająca na ustawienie nazwy hosta.
+     *
+     * @param hostName Nazwa hosta.
+     */
     public void setHostName(String hostName) {
         this.hostName = hostName;
     }
 
+    /**
+     * @return Nazwa użytkownika.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Metoda pozwalająca na ustawienie nazwy użytkownika.
+     *
+     * @param userName Nazwa użytkownika.
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * @return Hasło użytkownika
+     */
     public String getUserPassword() {
         return userPassword;
     }
 
+    /**
+     * Metoda pozwalająca na ustawienie hasła użytkownika.
+     *
+     * @param userPassword Hasło użytkownia
+     */
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
 
+    /**
+     * @return Port interfejsu AMI.
+     */
     public int getHostPort() {
         return hostPort;
     }
 
+    /**
+     * Metoda pozwalająca na ustawienie portu interfejsu AMI.
+     *
+     * @param hostPort Port interfejsu AMI.
+     */
     public void setHostPort(int hostPort) {
         this.hostPort = hostPort;
     }
