1fb301acae03edfc20d1794e67b2624aa572e0a5
Krzysztof <krz@ychu.pl>
2015-01-16 22:16:10 +0100

    Dodałem możliwość wysłania więcej niż jednej zmiennej o takiej samej nazwie w akcji

--- src/pl/ychu/asterisk/manager/UnifiedAction.java
+++ src/pl/ychu/asterisk/manager/UnifiedAction.java
@@ -1,7 +1,9 @@
 package pl.ychu.asterisk.manager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by krzysztof on 15.11.14.
@@ -9,29 +11,56 @@ import java.util.Iterator;
 public class UnifiedAction extends Action {
 
     private String actionName;
-    private HashMap<String, String> variables;
+    private List<Variable> variableList;
 
     public UnifiedAction(String actionName) {
         this.actionName = actionName;
-        variables = new HashMap<String, String>();
+        variableList = new ArrayList<Variable>();
     }
 
     public void putVariable(String name, String value) {
-        variables.put(name, value);
+        variableList.add(new Variable(name, value));
     }
 
     public void removeVariable(String name) {
-        variables.remove(name);
+        for (Variable variable : variableList) {
+            if (variable.getName().equals(name))
+                variableList.remove(variable);
+        }
     }
 
     @Override
     protected String getMessage() {
         String message = "Action: " + actionName + "\n";
-        Iterator i = variables.entrySet().iterator();
-        while (i.hasNext()) {
-            HashMap.Entry<String, String> entry = (HashMap.Entry<String, String>) i.next();
-            message += entry.getKey() + ": " + entry.getValue() + "\n";
+        for (Variable variable : variableList) {
+            message += variable.getName() + ": " + variable.getValue() + "\n";
         }
         return message;
+    }
+
+    class Variable {
+        private String name;
+        private String value;
+
+        public Variable(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
     }
 }
