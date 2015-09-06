675c37a01b9b6a8bdb10809102fd914fff0db587
Krzysztof Mazur <krz@ychu.pl>
2015-09-06 18:44:13 +0200

    remove high cpu usage command

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-docker run -p 5038:5038 --name asterisk_1 -d asterisk /bin/sh -c "while true; do ping 8.8.8.8; done"
+docker run -p 5038:5038 --name asterisk_1 -d asterisk /bin/sh -c "while true; do sleep 1; done"
 docker start asterisk_1
 docker exec asterisk_1 asterisk
